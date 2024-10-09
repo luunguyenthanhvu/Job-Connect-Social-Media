62021059ca7c168ad62a54744edde88be0484b0a
luunguyenthanhvu <21130612@st.hcmuaf.edu.vn>
2024-10-09 11:51:30 +0700

    PUSH source code

---
+++ user-service/src/test/java/vuluu/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vuluu.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
