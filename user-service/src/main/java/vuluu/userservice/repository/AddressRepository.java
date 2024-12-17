85830d851f249ff9809c9ac904d4c8ba7b292e05
luunguyenthanhvu <21130612@st.hcmuaf.edu.vn>
2024-12-17 16:47:59 +0700

    PUSH code create address for new user/ employer

--- user-service/src/main/java/vuluu/userservice/repository/AddressRepository.java
+++ user-service/src/main/java/vuluu/userservice/repository/AddressRepository.java
@@ -1,10 +1,13 @@
 package vuluu.userservice.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import vuluu.userservice.entity.Address;
+import vuluu.userservice.entity.User;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
 
+  List<Address> findAllByUser(User user);
 }
