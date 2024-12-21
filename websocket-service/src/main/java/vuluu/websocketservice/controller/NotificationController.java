29daa7776ee9d89236331bec9868b801c02a62a3
luunguyenthanhvu <21130612@st.hcmuaf.edu.vn>
2024-12-22 03:05:26 +0700

    UPDATE SOURCE

--- websocket-service/src/main/java/vuluu/websocketservice/controller/NotificationController.java
+++ websocket-service/src/main/java/vuluu/websocketservice/controller/NotificationController.java
@@ -6,14 +6,12 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
-import vuluu.websocketservice.configuration.WebSocketHandler;
 import vuluu.websocketservice.dto.request.UserRegisterRequestDTO;
 
 @Controller
 @RequiredArgsConstructor
 public class NotificationController {
 
-  private final WebSocketHandler webSocketHandler;
 
   @MessageMapping("/user.addUser")
   @SendTo("/user/topic")
@@ -21,11 +19,7 @@ public class NotificationController {
       SimpMessageHeaderAccessor headerAccessor) {
     String userId = requestDTO.getUserId();  // Giả sử bạn có trường userId trong requestDTO
     String message = "User connected: " + userId;
-    System.out.println("session id");
-    System.out.println(headerAccessor.getSessionId());
-    // Lưu userId vào WebSocketSession
     headerAccessor.getSessionAttributes().put("userId", userId);
-    // Gửi thông báo đến tất cả các session của user
-    webSocketHandler.sendNotification(userId, message);
+
   }
 }
